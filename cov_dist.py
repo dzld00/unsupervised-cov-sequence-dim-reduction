360b0163774448ad7fc19d84f806d937644291ec
dzld00 <38360844+dzld00@users.noreply.github.com>
2019-09-15 08:08:23 +0800

    Add files via upload

---
+++ cov_dist.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 14 19:11:37 2019
+
+@author: dmy
+"""
+
+import numpy as np
+from numpy.linalg import inv, norm, det
+from scipy.linalg import logm
+from functools import reduce 
+
+
+def cov_dist_unit(punit1, punit2):
+    p12 = np.sqrt(reduce(np.dot, [inv(punit1), np.square(punit2), inv(punit1)]))
+    
+    A12 = logm(p12)
+    
+    return norm(A12)
+    
+def cov_dist(p1, p2):
+    n = p1.shape[0]
+    
+    pdet1 = det(p1)
+    pdet2 = det(p2)
+    
+    punit1 = p1 / (np.power(pdet1, (1/n)))
+    punit2 = p2 / (np.power(pdet2, (1/n)))
+    
+    square_d = np.square(cov_dist_unit(punit1, punit2)) + np.multiply((1 / n), np.square(np.log(pdet1) - np.log(pdet2)))
+    
+    return np.sqrt(square_d)
